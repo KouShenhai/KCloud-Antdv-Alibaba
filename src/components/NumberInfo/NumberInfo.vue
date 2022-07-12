bb25187a9fb07bd951c4be110a6227ad9ece1c2c
k <2413176044@qq.com>
2022-07-12 01:00:21 +0000

    update src/components/NumberInfo/NumberInfo.vue.

--- src/components/NumberInfo/NumberInfo.vue
+++ src/components/NumberInfo/NumberInfo.vue
@@ -1,54 +1,56 @@
+<!--
 <template>
-  <div :class="[prefixCls]">
-    <slot name="subtitle">
-      <div :class="[`${prefixCls}-subtitle`]">{{ typeof subTitle === 'string' ? subTitle : subTitle() }}</div>
-    </slot>
-    <div class="number-info-value">
-      <span>{{ total }}</span>
-      <span class="sub-total">
+    <div :class="[prefixCls]">
+        <slot name="subtitle">
+            <div :class="[`${prefixCls}-subtitle`]">{{ typeof subTitle === 'string' ? subTitle : subTitle() }}</div>
+        </slot>
+        <div class="number-info-value">
+            <span>{{ total }}</span>
+            <span class="sub-total">
         {{ subTotal }}
         <icon :type="`caret-${status}`" />
       </span>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import Icon from 'ant-design-vue/es/icon'
+    import Icon from 'ant-design-vue/es/icon'
 
-export default {
-  name: 'NumberInfo',
-  props: {
-    prefixCls: {
-      type: String,
-      default: 'ant-pro-number-info'
-    },
-    total: {
-      type: Number,
-      required: true
-    },
-    subTotal: {
-      type: Number,
-      required: true
-    },
-    subTitle: {
-      type: [String, Function],
-      default: ''
-    },
-    status: {
-      type: String,
-      default: 'up'
+    export default {
+        name: 'NumberInfo',
+        props: {
+            prefixCls: {
+                type: String,
+                default: 'ant-pro-number-info'
+            },
+            total: {
+                type: Number,
+                required: true
+            },
+            subTotal: {
+                type: Number,
+                required: true
+            },
+            subTitle: {
+                type: [String, Function],
+                default: ''
+            },
+            status: {
+                type: String,
+                default: 'up'
+            }
+        },
+        components: {
+            Icon
+        },
+        data () {
+            return {}
+        }
     }
-  },
-  components: {
-    Icon
-  },
-  data () {
-    return {}
-  }
-}
 </script>
 
 <style lang="less" scoped>
-  @import "index";
+    @import "index";
 </style>
+-->
