46d43ffda8796121a76f2b86fb24416fde52cb30
koushenhai <2413176044@qq.com>
2022-07-17 23:09:06 +0800

    更新日志文件

--- src/views/index.vue
+++ src/views/index.vue
@@ -251,8 +251,7 @@ export default {
         '</if>\n' +
         '```\n' +
         '### 联系\n' +
-        '如果讨论后端技术，推荐添加：\n' +
-        '[![加入QQ群](https://img.shields.io/badge/Q群-218686225-blue.svg)](https://qm.qq.com/cgi-bin/qm/qr?k=WFANTXDEjrDw6UxsrRFCv_rQsEu6LTxH&jump_from=webapi)\n'
+        '后端技术交流群 [![加入QQ群](https://img.shields.io/badge/Q群-218686225-blue.svg)](https://qm.qq.com/cgi-bin/qm/qr?k=WFANTXDEjrDw6UxsrRFCv_rQsEu6LTxH&jump_from=webapi)'
     }
   },
   computed: {
