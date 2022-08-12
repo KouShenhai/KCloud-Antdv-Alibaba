a684084befeec4a38806b78feb92428183ed313f
koushenhai <2413176044@qq.com>
2022-08-12 23:14:40 +0800

    优化代码

--- src/api/sys/load.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-// 新增字典类型
-export function login (params) {
-  return request({
-    url: 'http://175.178.69.253/oauth2/oauth/token' + params,
-    method: 'post'
-  })
-}
