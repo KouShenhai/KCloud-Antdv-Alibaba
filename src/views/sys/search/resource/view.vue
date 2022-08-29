931a8d427e871613199814c700d8c62df6119ab9
koushenhai <2413176044@qq.com>
2022-08-29 23:13:14 +0800

    优化代码

--- src/views/sys/search/resource/view.vue
+++ src/views/sys/search/resource/view.vue
@@ -1,16 +1,58 @@
 <template>
+  <a-card :bordered="false">
   <div class="table-page-search-wrapper">
+    <a-form layout="inline">
       <a-row :gutter="48">
         <a-col :md="24" :sm="24">
-            <a-input allow-clear/>
+          <a-input v-model="keyword" placeholder="请输入搜索内容" allow-clear >
+            <a-icon slot="prefix" type="search" :style="{ color: 'rgba(0,0,0,.25)' }"/>
+          </a-input>
         </a-col>
       </a-row>
-  </div>
+    </a-form>
+  </div><br/>
+
+    <div>11111111111</div>
+    <div>333333333</div>
+    <div>55555</div>
+    <div>777777777</div><br/>
+
+    <div>11111111111</div>
+    <div>333333333</div>
+    <div>55555</div>
+    <div>777777777</div><br/>
+
+    <div>11111111111</div>
+    <div>333333333</div>
+    <div>55555</div>
+    <div>777777777</div><br/>
+
+    <div>11111111111</div>
+    <div>333333333</div>
+    <div>55555</div>
+    <div>777777777</div><br/>
+
+    <div>11111111111</div>
+    <div>333333333</div>
+    <div>55555</div>
+    <div>777777777</div><br/>
+
+    <div>11111111111</div>
+    <div>333333333</div>
+    <div>55555</div>
+    <div>777777777</div><br/>
+
+  </a-card>
 </template>
 
 <script>
     export default {
-        name: "Search-Resource"
+        name: "Search-Resource",
+      data () {
+        return {
+          keyword: ""
+        }
+      }
     }
 </script>
 
