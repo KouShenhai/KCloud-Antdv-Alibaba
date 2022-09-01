94b3bf66108ca9f394a71552ab1880f924bca746
koushenhai <2413176044@qq.com>
2022-09-01 09:18:11 +0800

    优化代码

--- src/views/sys/search/resource/view.vue
+++ src/views/sys/search/resource/view.vue
@@ -6,6 +6,9 @@
       </a-input>
     </div>
     <br/>
+    <div v-if="list.length > 0">
+      <span style="font-size: 14px;font-weight: bolder;">搜索 <span style='color:red;'>{{searchWord}}</span> 找到 <span style='color:red;'>{{total}}</span> 个相关内容</span>
+    </div><br/>
     <div v-for="(item, i) in list" :key="i">
       <div style="font-size: 14px;font-weight: bolder;" v-html="item.title"/>
       <span style="font-size: 14px;" v-html="item.remark"/>
@@ -24,6 +27,8 @@
       data () {
         return {
           keyword: "",
+          total: "",
+          searchWord: "",
           form: {
             pageNum: 1,
             pageSize: 10000,
@@ -49,11 +54,13 @@
       },
       methods: {
           searchResource() {
+            this.searchWord = this.keyword
             this.form.queryStringList = []
             this.form.queryStringList.push({field: "remark", value: this.keyword});
             this.form.queryStringList.push({field: "title", value: this.keyword});
             searchResource(this.form).then(response => {
                 this.list = response.data == null ? [] : response.data.records
+                this.total = response.data == null ? 0 : response.data.total - 0
               }
             )
           }
