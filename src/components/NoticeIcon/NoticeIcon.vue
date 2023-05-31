b4ea78794dd5539410b584b0a301c194d24d51fd
koushenhai <2413176044@qq.com>
2023-06-01 00:47:45 +0800

    feat: 验签

--- src/components/NoticeIcon/NoticeIcon.vue
+++ src/components/NoticeIcon/NoticeIcon.vue
@@ -143,7 +143,7 @@ export default {
       if (typeof WebSocket === 'undefined') {
         console.log('您的浏览器不支持WebSocket')
       } else {
-        let ip = '192.168.62.1:7777'
+        let ip = '127.0.0.1:7777'
         let protocol = 'ws'
         let url = ''
         if (window.location.protocol === 'https') {
