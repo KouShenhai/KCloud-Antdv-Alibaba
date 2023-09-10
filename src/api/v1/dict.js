41d25cc66941912b699441d9db030b7faa9b91b4
koushenhai <2413176044@qq.com>
2023-09-10 14:21:30 +0800

    feat: 对接 dict

---
+++ src/api/v1/dict.js
@@ -0,0 +1,52 @@
+import request from '@/utils/request'
+
+// 查询字典列表
+export function listDict (query) {
+  return request({
+    url: '/admin/v1/dicts/list',
+    method: 'post',
+    data: query
+  })
+}
+
+// 根据类型查询字典
+export function listDictByType (type) {
+  return request({
+    url: '/admin/v1/dicts/' + type + '/option-list',
+    method: 'get'
+  })
+}
+
+// 查看字典
+export function getDictById (id) {
+  return request({
+    url: '/admin/v1/dicts/' + id,
+    method: 'get'
+  })
+}
+
+// 新增字典
+export function insertDict (data) {
+  return request({
+    url: '/admin/v1/dicts',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改字典
+export function updateDict (data) {
+  return request({
+    url: '/admin/v1/dicts',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除字典
+export function deleteDictById (id) {
+  return request({
+    url: '/admin/v1/dicts/' + id,
+    method: 'delete'
+  })
+}
