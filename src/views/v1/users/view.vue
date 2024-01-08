c0696cea87c325ead64d8b92726c16c49f57bed9
koushenhai <2413176044@qq.com>
2024-01-08 12:00:17 +0800

    feat: 提交代码

--- src/views/v1/users/view.vue
+++ src/views/v1/users/view.vue
@@ -9,7 +9,7 @@
               <a-row :gutter="48">
                 <a-col :md="8" :sm="24">
                   <a-form-item label="用户名">
-                    <a-input v-model="queryParam.username" placeholder="请输入用户名(不支持模糊查询)" allow-clear />
+                    <a-input v-model="queryParam.username" placeholder="请输入用户名" allow-clear />
                   </a-form-item>
                 </a-col>
                 <a-col :md="8" :sm="24">
